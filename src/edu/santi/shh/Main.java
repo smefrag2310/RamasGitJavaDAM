b4aa29c7e033fac61cf450ca298150f5a7f74ca4
smefrag2310 <smefrag2310@g.educaand.es>
2025-11-14 14:11:13 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal (SSH listo)

---
+++ src/edu/santi/shh/Main.java
@@ -0,0 +1,20 @@
+package edu.santi.shh;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        //Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
+        Random random = new Random();
+        int n,i;
+        System.out.println("Números de la Lotería Primitiva:");
+        // Genera 5 números principales
+        for(i=1;i<=5;i++) {
+        n=random.nextInt(99)+1;
+        System.out.printf("%d ",n);
+        }
+        // Número complementario
+        n=random.nextInt(99)+1;
+        System.out.printf("\nEl número complementario: %d\n",n);
+    }
+}
